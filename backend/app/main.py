85915e95f13d1b835fc561730a4e7ebe2ac1f5ef
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-09-20 09:34:14 -0700

    frontend-backend integration tested

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add.middleware(
+app.add_middleware(
   CORSMiddleware,
   allow_origins=["http://localhost:5173"],
   allow_credentials=True,
