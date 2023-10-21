9771c6354cd6dc6dd21981ed01fa8e606bcd06d6
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-21 13:20:37 -0700

    .env setup for frontend - reinstalled backend for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import tutoralogo from "./assets/tutora.png";
 
 function App() {
   return (
+    // any html code in between will reflect on the landing page
     <div className="flex h-screen">
       <div className="m-auto">
         <h1 className="flex justify-center text-3xl font-bold underline text-white">
@@ -10,6 +11,7 @@ function App() {
         <img className="mt-8" src={tutoralogo} alt="Tutora Logo" />
       </div>
     </div>
+    // any html code in between will reflect on the landing page
   );
 }
 
