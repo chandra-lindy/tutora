d4acba6250d45c39623cef59de5d91f9332cc987
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-11-07 14:09:52 -0800

    created aws cognito - prep for authentication

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,11 @@
+// configuration for aws cognito
+const awsconfig = {
+  aws_project_region: "us-west-2",
+  aws_cognito_identity_pool_id:
+    "us-west-2:b009e503-d6a4-4f29-bb59-295d3972dc81",
+  aws_cognito_region: "us-west-2",
+  aws_user_pools_id: "us-west-2_ugwDDfM0F",
+  aws_user_pools_web_client_id: "1em6gim47lilrjivc83st1mg02",
+};
+
+export default awsconfig;
