d762d46ca5eb5672407eea33ff5e0bbcc3983088
vshyyan <valentynashyyan@gmail.com>
2023-11-14 12:36:26 -0800

    implemented chat

--- backend/app/chat.py
+++ backend/app/chat.py
@@ -1,2 +1,16 @@
-def chat():
-  return "Hello! This is AI message"
+from openai import OpenAI
+
+client = OpenAI()
+messages=[{"role": "system", "content": "You are a tutor to a student. You are supposed to guide the student when they ask quesitons"},
+            {"role": "assistant", "content": "Hello! How can I help you today?"}]
+def chat(messages):
+  chat_completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[{"role": m["role"], "content": m["content"]} for m in messages],
+  stream = True
+  )
+  responce = chat_completion['choices'][0]['message']['content']
+  return responce
+
+def addMessage(data):
+  messages.append({"role": "user", "content": data})
